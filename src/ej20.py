afec6655f8ec9e6b0ebeabf0ac6069f7b78e13cb
abracal <abravoc@iesrafaelalberti.es>
2024-10-22 18:14:08 +0200

    faltatiempo

--- src/ej20.py
+++ src/ej20.py
@@ -5,3 +5,41 @@
 # continuar. Si se encuentra una coincidencia, indicar en qué posición se encontró y finalizar la ejecución.
 
 
+
+# Entrada
+def pregunta():
+    frase = input("Dime una frase: ")
+    vocal = input("Dime una letra: ")
+    return frase, vocal
+
+
+
+# Procesamiento
+def comprobar(frase, vocal):
+    contador = 0
+    for vocal in frase:
+        contador = contador + 1
+    return contador
+
+def Anti_Tontos(vocal):
+    if len(vocal) != 1:
+        return False
+    else:
+        return True
+
+
+
+# Salida
+
+
+def main():
+    frase, vocal = pregunta()
+    dudoso = Anti_Tontos(vocal)
+    if dudoso == True:
+        contador = comprobar(frase, vocal)
+        print(f"La letra introducida ha sido repetida {contador} veces en la frase")
+    else:
+        print("Te he dicho que escribas solo una letra no más")
+    
+    
+main()
---
+++ src/ej21.py
@@ -0,0 +1,62 @@
+#Crear un programa que permita al usuario ingresar los montos de las compras
+# de un cliente (se desconoce la cantidad de datos que cargará, la cual puede
+# cambiar en cada ejecución), cortando el ingreso de datos cuando el usuario
+# ingrese el monto 0. Si ingresa un monto negativo, no se debe procesar y se
+# debe pedir que ingrese un nuevo monto. Al finalizar, informar el total a
+# pagar teniendo que cuenta que, si las ventas superan el total de $1000, se le debe aplicar un 10% de descuento.
+
+#Entrada
+
+def pregunta():
+    dinerito = "0"
+    try:
+        dinerito = int((input("Dime cuanto te ha costado este articulo, para parar el programa ingrese 0: ")))
+    except:
+        print("Por favor escriba un número")
+        return pregunta()
+    return dinerito
+
+
+
+# Procesamiento
+resultado = 0
+def comprobacion(dinero):
+    if dinero > 1000:
+        return dinero - (dinero / 10)
+    else:
+        return dinero
+
+    
+def Anti_Robo(dinero):
+    if dinero > 0:
+        return True
+    else:
+        return False
+
+
+
+
+
+
+
+#Salida
+
+def main():
+    totalito = 0
+    dinero = 1
+    resultado = comprobacion(dinero)
+    valor = Anti_Robo(dinero)
+    if valor == True:
+        while dinero != 0:
+            dinero = pregunta()
+            if dinero > 0:
+                totalito += dinero
+            else:
+                print("Monto inválido. Ingrese un valor positivo.")
+            resultado = comprobacion(totalito)
+            print(f"Esta compra te va a costar: {resultado}")
+    else:
+        print("Por favor escriba números mayores a 0")         
+
+main()
+
